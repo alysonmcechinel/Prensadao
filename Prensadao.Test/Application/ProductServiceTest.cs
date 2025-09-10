600470d79e76fe27eab46d6e864ac34349a37653
alysonmcechinel <alysonmcechinell@gmail.com>
2025-09-09 21:54:41 -0300

    add test: method enabled

--- Prensadao.Test/Application/ProductServiceTest.cs
+++ Prensadao.Test/Application/ProductServiceTest.cs
@@ -195,6 +195,7 @@ public class ProductServiceTest
     [Fact]
     public async Task Update_When_IDIsNull()
     {
+        // Arrange
         var repository = A.Fake<IProductRepository>();
         var productService = new ProductService(repository);
 
@@ -205,8 +206,6 @@ public class ProductServiceTest
             Enabled = true,
             Value = 10.50m
         };
-
-        //A.CallTo(() => repository.GetById(dto.ProductId!.Value)).Returns(Task.FromResult<Product?>(null)!);
 
         // Act
         var act = () => productService.Update(dto);
@@ -220,6 +219,7 @@ public class ProductServiceTest
     [Fact]
     public async Task Update_When_ProductNotFound()
     {
+        // Arrange
         var repository = A.Fake<IProductRepository>();
         var productService = new ProductService(repository);
 
@@ -236,6 +236,54 @@ public class ProductServiceTest
 
         // Act
         var act = () => productService.Update(dto);
+
+        // Assert
+        await Assert.ThrowsAsync<ArgumentException>(async () => await act());
+        A.CallTo(() => repository.GetById(A<int>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => repository.Update(A<Product>._)).MustNotHaveHappened();
+    }
+
+    [Fact]
+    public async Task Enabled_When_Success()
+    {
+        // Arrange
+        var repository = A.Fake<IProductRepository>();
+        var productService = new ProductService(repository);
+
+        var dto = new ProductEnabledDto
+        {
+            ProductId = 1,
+            Enabled = true
+        };
+
+        // Act
+        await productService.Enabled(dto);
+
+        // Assert
+        A.CallTo(() => repository.GetById(A<int>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => repository.Update(A<Product>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => repository.Update(
+            A<Product>.That.Matches(p =>
+            p.Enabled == dto.Enabled
+            && p.ProductId == dto.ProductId)));
+    }
+
+    [Fact]
+    public async Task Enabled_When_ProductNotFound()
+    {
+        // Arrange
+        var repository = A.Fake<IProductRepository>();
+        var productService = new ProductService(repository);
+        var dto = new ProductEnabledDto
+        {
+            ProductId = 1,
+            Enabled = true
+        };
+
+        A.CallTo(() => repository.GetById(dto.ProductId)).Returns(Task.FromResult<Product?>(null)!);
+
+        // Act
+        var act = () => productService.Enabled(dto);
 
         // Assert
         await Assert.ThrowsAsync<ArgumentException>(async () => await act());
