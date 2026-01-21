d1ded74299e93e9e61a8c839883464d5157368ec
alysonmcechinel <alysonmcechinell@gmail.com>
2026-01-20 22:10:56 -0300

    alter: phone property type to string in Customers table

---
+++ Prensadao.Infra/Persistence/Migrations/20260121010825_fix-telefone.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prensadao.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixtelefone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Phone",
+                table: "Customers",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
